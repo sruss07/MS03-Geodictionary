ef50670ff54a22e5da03a1de904b224d3881cc07
sruss07 <srussell1903@yahoo.co.uk>
2021-03-10 08:59:58 +0000

    updated to allow app to link to mongodb

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from flask import (
-    Flask, flash, render_template, 
+    Flask, flash, render_template,
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
@@ -13,7 +13,7 @@ app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
-app.scret_key = os.environ.get("SECRET_KEY")
+app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
 
@@ -21,8 +21,8 @@ mongo = PyMongo(app)
 @app.route("/")
 @app.route("/get_geoTerms")
 def get_geoTerms():
-     # Sorts terms alphabetically
-    geoTerms = list(mongo.db.geoTerms.find().sort("geoTerms"))
+    # Sorts terms alphabetically
+    geoTerms = mongo.db.geoTerms.find()
     return render_template("terms.html", geoTerms=geoTerms)
 
 
