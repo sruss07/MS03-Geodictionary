ae23041a17bf1bc565c47b9269f449fb8dfa4990
sruss07 <srussell1903@yahoo.co.uk>
2021-03-23 11:49:48 +0000

    added js for page scrolling

--- static/js/script.js
+++ static/js/script.js
@@ -2,4 +2,5 @@ $(document).ready(function(){
     $(".sidenav").sidenav({edge: "right"});
     $('.collapsible').collapsible();
     $('select').formSelect();
+    $('.fixed-action-btn').floatingActionButton();
 });
