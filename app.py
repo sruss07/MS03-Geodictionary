83faed772ad7bcd9a1b97589e21196c0d9eb5a0d
sruss07 <srussell1903@yahoo.co.uk>
2021-03-28 15:54:41 +0000

    added 404 error page

--- app.py
+++ app.py
@@ -189,6 +189,11 @@ def delete_term(term_id):
     return redirect(url_for("get_geoTerms"))
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
